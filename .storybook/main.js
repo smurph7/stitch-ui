da6454da51732acd67407e8d3a6dabc0924f3a89
Sarah <sarahnmurphy93@gmail.com>
2021-11-04 18:52:07 +1100

    Remove unused files

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,10 +4,10 @@ module.exports = {
     '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'
   ],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
-    '@storybook/addon-docs',
     '@storybook/addon-a11y'
   ]
 };
