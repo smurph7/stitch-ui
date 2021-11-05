e9f17d20f4685940ae45285a313b19181dc2c200
Sarah <sarahnmurphy93@gmail.com>
2021-11-05 17:37:19 +1100

    Add bundle for publishing to npm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export { Box, Button } from './components';
+import { Box, Button, Container, Flex, Grid } from './components';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverClose
+} from './components/Popover';
+
+export {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Grid,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverClose
+};
